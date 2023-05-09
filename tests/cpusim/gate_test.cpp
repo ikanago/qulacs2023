3b827758ee4c5bfcf214ef79dca32e29937979b5
ikanago <ikanago-dev@protonmail.com>
2023-05-09 05:08:46 +0000

    Refactor test

--- tests/cpusim/gate_test.cpp
+++ tests/cpusim/gate_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <Eigen/Core>
 #include <core/random.hpp>
 #include <core/types.hpp>
 #include <cpusim/gate.hpp>
@@ -10,28 +11,33 @@
 
 const auto eps = 1e-12;
 
-testing::AssertionResult run_random_gate_apply(std::function<QuantumGate<Runtime::CPU>(UINT)>) {}
-
-TEST(GateTest, ApplySingleQubitGate) {
-    const auto X = make_X();
-    const UINT n_qubits = 5;
-    const UINT dim = 1ULL << n_qubits;
+template <class QuantumGateConstructor>
+void run_random_gate_apply(
+    UINT n_qubits,
+    QuantumGateConstructor constructor,
+    std::function<Eigen::MatrixXcd()> matrix_factory
+) {
+    const auto matrix = matrix_factory();
+    const ITYPE dim = 1ULL << n_qubits;
     Random random;
 
     Eigen::VectorXcd test_state = Eigen::VectorXcd::Zero(dim);
-    for (UINT repeat = 0; repeat < 10; ++repeat) {
+    for (UINT repeat = 0; repeat < 10; repeat++) {
         auto state = StateVector<Runtime::CPU>::Haar_random_state(n_qubits);
-        for (ITYPE i = 0; i < dim; ++i) test_state[i] = state[i];
+        for (ITYPE i = 0; i < dim; i++) test_state[i] = state[i];
 
-        auto mat = X;
-        UINT target = random.int32() % n_qubits;
-        auto gate = QuantumGate<Runtime::CPU>::X(target);
+        const UINT target = random.int32() % n_qubits;
+        auto gate = constructor(target);
         gate.update_quantum_state(state);
 
-        test_state = get_expanded_eigen_matrix_with_identity(target, mat, n_qubits) * test_state;
+        test_state = get_expanded_eigen_matrix_with_identity(target, matrix, n_qubits) * test_state;
 
-        for (ITYPE i = 0; i < dim; ++i) {
+        for (ITYPE i = 0; i < dim; i++) {
             ASSERT_NEAR(std::abs(state[i] - test_state[i]), 0, eps);
         }
     }
+}
+
+TEST(GateTest, ApplySingleQubitGate) {
+    run_random_gate_apply(5, QuantumGate<Runtime::CPU>::X, make_X);
 }
